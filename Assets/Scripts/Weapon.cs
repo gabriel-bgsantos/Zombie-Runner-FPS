3e23758340937d4a59ed7f62d730d52f8daa4ef9
gpw pc <gabrielbgsantos@hotmail.com>
2023-02-09 08:01:44 -0300

    Shooting hit effect

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,7 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] float range = 100f;
     [SerializeField] int hitDamage = 10;
     [SerializeField] ParticleSystem muzzleFlash;
-
+    [SerializeField] GameObject hitEffect; // gameObject because it needs to be instantiated
 
     void Update()
     {
@@ -26,7 +26,7 @@ public class Weapon : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range)) { // origin, direction, hit, range
             Debug.Log("I hit this thing: " + hit.transform.name);
-            // TO DO: add some hit effect 
+            CreateHitImpact(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
             if (target == null) return;
             target.TakeDamage(hitDamage);
@@ -38,5 +38,10 @@ public class Weapon : MonoBehaviour
 
     void PlayMuzzleFlash() {
         muzzleFlash.Play();
+    }
+
+    void CreateHitImpact(RaycastHit hit) {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal)); //hit.point = location, quaternion(where the particle will look at)
+        Destroy(impact, 1);
     }
 }
